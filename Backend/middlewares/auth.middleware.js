ee28b75c5c3bb1748dedb2a733b413f5e8d6be6e
manitejabayyaa <maniteja.bayya369@gmail.com>
2025-03-18 20:52:53 +0530

    changed the middleware

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -5,7 +5,6 @@ const jwt = require('jsonwebtoken');
 module.exports.authUser = async (req, res, next) => {
     const token = req.cookies.token || req.headers.authorization.split(' ')[1];
     if (!token) {
-
         return res.status(401).json({ message: "Unauthorized" });
     }
 
